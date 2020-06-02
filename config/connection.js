87c667f0764e894576f45eaa8cc47e37a5a38683
Cris M <cristian.montenegro.p@gmail.com>
2020-06-01 23:25:17 -0700

    adding JawsDB connection info

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,22 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 require("dotenv").config();
+var connection;
 
-var connection = mysql.createConnection({
-  host: process.env.MYSQL_HOST,
-  port: process.env.MYSQL_PORT,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: "burgers_db",
-});
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+} else {
+  connection = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: "burgers_db",
+  });
+}
+
 
 // Make connection.
 connection.connect(function (err) {
